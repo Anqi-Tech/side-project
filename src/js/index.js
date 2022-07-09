e35e4ab796eb3c62b225babb29d7bb5c65fed257
Anqi-Tech <71527898+Anqi-Tech@users.noreply.github.com>
2022-07-09 01:17:25 -0400

    this looks so messy

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,47 @@
-//import { gameDialogue } from "./dialogue";
-
 const startButton = document.getElementById("start-btn");
 const startPage = document.getElementById("start-page");
 const gamePage = document.getElementById("game-page");
-//const dialogueElement = document.getElementById("dialogue");
-//const nextButton = document.getElementById("next-btn");
+const dialogueElement = document.getElementById("dialogue");
+const nextButton = document.getElementById("next-btn");
+const annoyingPlaceholder = document.getElementById("invisible");
+const choiceOne = document.getElementById("choice-one");
+const choiceTwo = document.getElementById("choice-two");
 
-//let currentDialogueIndex = 0;
+let currentDialogueIndex = 0;
 
 startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentDialogueIndex++;
+  setNextDialogue();
+});
 
 function startGame() {
   startPage.classList.add("hide");
   gamePage.classList.remove("hide");
+  setNextDialogue();
 }
+
+function setNextDialogue() {
+  if (currentDialogueIndex == 3) {
+    firstChoice();
+  } else {
+    showDialogue(gameDialogue[currentDialogueIndex]);
+  }
+}
+
+function showDialogue(text) {
+  dialogueElement.innerText = text.text;
+}
+
+function firstChoice() {
+  nextButton.classList.add("hide");
+  annoyingPlaceholder.classList.add("hide");
+  choiceOne.classList.remove("hide");
+  choiceTwo.classList.remove("hide");
+}
+
+const gameDialogue = [
+  { text: "Hello." },
+  { text: "So..." },
+  { text: "Are you here to teach me Vue?" },
+];
