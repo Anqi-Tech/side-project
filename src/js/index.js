5aafb39b6c6addf04b0939f8dc03fd08ba2e1200
Anqi-Tech <71527898+Anqi-Tech@users.noreply.github.com>
2022-07-10 17:06:43 -0400

    i gave up lets just hide and show images instead

--- src/js/index.js
+++ src/js/index.js
@@ -62,10 +62,10 @@ function happyResponse() {
   showDialogue(gameDialogue[currentDialogueIndex]);
   nextButton.classList.remove("hide");
   annoyingPlaceholder.classList.remove("hide");
+  document.getElementById("img-happy").classList.remove("hide");
+  document.getElementById("img-neutral").classList.add("hide");
   choiceOne.classList.add("hide");
   choiceTwo.classList.add("hide");
-  const image = document.getElementById("game-img");
-  image.src = "images/neutral.png";
 }
 
 function startClicker() {
@@ -81,20 +81,27 @@ function showClicks() {
 
 function clickMilestones() {
   switch (true) {
-    case clickCount == 10:
-      gamePage.style.backgroundColor = "green";
-      break;
-
     case clickCount == 25:
-      gamePage.style.backgroundColor = "blue";
+      document.getElementById("img-happy").classList.add("hide");
+      document.getElementById("img-vg1").classList.remove("hide");
       break;
 
     case clickCount == 50:
-      gamePage.style.backgroundColor = "yellow";
+      document.getElementById("img-vg1").classList.add("hide");
+      document.getElementById("img-vg2").classList.remove("hide");
+      break;
+
+    case clickCount == 75:
+      document.getElementById("img-vg2").classList.add("hide");
+      document.getElementById("img-vg3").classList.remove("hide");
       break;
 
     case clickCount == 100:
-      gamePage.style.backgroundColor = "red";
+      goodEnding();
       break;
   }
+}
+
+function goodEnding() {
+  gamePage.classList.add("hide");
 }
